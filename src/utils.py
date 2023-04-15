0487fbc6d77331b32a5c0d874df14b45fde6e7fd
chaitanya <chaitanyaravuri04@gmail.com>
2023-04-15 15:31:52 +0530

    PREDICTION PIPELINE

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import dill
+import pickle
 
 import numpy as np
 import pandas as pd
@@ -55,7 +56,13 @@ def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e,sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
 
 
     # here model.keys()--> are the predicted values by the machine
