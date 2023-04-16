ecda94c3bba193174605545122241a83df4ab83e
chaitanya <chaitanyaravuri04@gmail.com>
2023-04-16 13:12:39 +0530

    SORTED EVERYTHING

--- src/exception.py
+++ src/exception.py
@@ -1,28 +1,23 @@
-# for handling exception
-
-import sys    #sys module provides various functions&variable used for manipulating different python runtime environment
+import sys
 from src.logger import logging
-
-
 # Defining a function to generate an error message with the details of the error.
-def error_message_detail (error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()  #exc_info gives execution info and gives 3imp info like which file,lineno
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()   #exc_info gives execution info and gives 3imp info like which file,lineno
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="error occured in the python script name [{0}] line number[{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
-    return error_message
-        
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
 
+    return error_message
 
 # Defining a custom exception class that inherits from the built-in Exception class.
+ 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):   # Defining the __init__ method to initialize the error message and error details.
-        super.__init__(error_message) # Calling the __init__ method of the parent class to initialize the exception object.
-        self.error_message=error_message_detail(error_message,error_detail=error_detail) # Generating the error message with the details of the error using the error_message_detail function.
-        
+    def __init__(self,error_message,error_detail:sys):  # Defining the __init__ method to initialize the error message and error details.
+        super().__init__(error_message)   # Calling the __init__ method of the parent class to initialize the exception object.
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)  # Generating the error message with the details of the error using the error_message_detail function.
+    
     def __str__(self):
         return self.error_message
-
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,15 @@
 #any functionality which we are writing in common way used in the entire application
 import os
 import sys
+
+import numpy as np 
+import pandas as pd
 import dill
 import pickle
-
-import numpy as np
-import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -18,14 +18,12 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
-
-
-def evaluate_model(x_train,y_train,x_test,y_test,models,param):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
@@ -34,16 +32,16 @@ def evaluate_model(x_train,y_train,x_test,y_test,models,param):
             para=param[list(models.keys())[i]]
 
             gs = GridSearchCV(model,para,cv=3)
-            gs.fit(x_train,y_train)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(x_train,y_train)
+            model.fit(X_train,y_train)
 
-            #model.fit(x_train, y_train)  # Train model
+            #model.fit(X_train, y_train)  # Train model
 
-            y_train_pred = model.predict(x_train)
+            y_train_pred = model.predict(X_train)
 
-            y_test_pred = model.predict(x_test)
+            y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
 
@@ -54,7 +52,7 @@ def evaluate_model(x_train,y_train,x_test,y_test,models,param):
         return report
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
 def load_object(file_path):
     try:
